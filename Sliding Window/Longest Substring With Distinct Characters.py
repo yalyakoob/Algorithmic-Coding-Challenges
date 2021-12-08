fdc2a2e2b5a088177324da4478223c839720bafe
Youssef Al-Yakoob <yalyakoob@gmail.com>
2021-12-08 15:16:13 -0800

    longest substring with distinct characters

---
+++ Sliding Window/Longest Substring With Distinct Characters.py
@@ -0,0 +1,17 @@
+"""Given a string, find the length of the longest substring, which has all distinct characters"""
+"""Example: String = "aabccbb", Output = 3"""
+
+
+def non_repeat_substring(str1):
+    window_start = 0
+    max_length = 0
+    char_index_map = {}
+    for window_end in range(len(str1)):
+        right_char = str1[window_end]
+        if right_char in char_index_map:
+            window_start = max(window_start, char_index_map[right_char] + 1)
+        char_index_map[right_char] = window_end
+        max_length = max(max_length, window_end - window_start + 1)
+    return max_length
+
+
